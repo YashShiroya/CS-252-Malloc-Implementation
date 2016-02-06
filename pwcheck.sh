11376bb98403de4ceeb7d5338875b8723363c995
yshiroya <yshiroya@purdue.edu>
2016-02-06 17:17:14 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -8,7 +8,7 @@ git commit -a -m "Lab 2 commit" >> .local.git.out
 strength=0
 password=$1
 length=${#password}
-if [ $length -gt 5 ]; then
-	echo "greater than 5";
+if [ { $length -gt 5 } -a { $length -lt 32} ]; then
+	echo "Works great!";
 fi
 echo Strength: $strength
