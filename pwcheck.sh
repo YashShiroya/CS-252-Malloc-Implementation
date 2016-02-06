8dc9394f4f76072da33edd3980a9e84cc69cb32e
yshiroya <yshiroya@purdue.edu>
2016-02-06 17:31:16 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -5,10 +5,17 @@ git add $0 >> .local.git.out
 git commit -a -m "Lab 2 commit" >> .local.git.out
 
 #Your code here
-strength=0aa
+strength=0
 password=$1
 length=${#password}
+
+#Check length requirements
 if [ $length -lt 6 -o $length -gt 32 ]; then
 	echo "Error: Password length invalid.";
 fi
+
+#Add strength for length of string
+let strength=strength+length;
+
+
 echo Strength: $strength
