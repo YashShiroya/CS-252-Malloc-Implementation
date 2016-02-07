988818e7fa384a5afa46f3cf3e39134f0899bbee
yshiroya <yshiroya@purdue.edu>
2016-02-07 03:34:35 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -18,7 +18,7 @@ else
 	let strength=strength+length;
 	
 #Check for special characters
-    if [ `egrep -q [#$\+%@] $password` | echo ]
+    if [ `egrep -q [#$\+%@] $password` ]
     then
     	let strength=strength+1	
 	fi	
