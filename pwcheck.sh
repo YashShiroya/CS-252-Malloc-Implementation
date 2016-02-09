451c5269946af5ea4bbfff725190d516d4088315
yshiroya <yshiroya@purdue.edu>
2016-02-08 21:40:51 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -43,7 +43,7 @@ then
   	echo "+5 points for alphabets"
 fi	
 
-printf "--------------"
+print "--------------"
 printf "Strength: %d  |\n--------------------------\n" $strength;
-printf "--------------"
+print "--------------"
 
