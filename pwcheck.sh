3704227a7877ba49bc4d570cd1c6e8285faef343
yshiroya <yshiroya@purdue.edu>
2016-02-10 14:19:13 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -45,11 +45,29 @@ then
   	echo "| +5 points for alphabets                                 |"
 fi	
 
+flag=0
 
-if [[ `egrep  '([0-9])\1+' $1` -o `egrep  '([a-z])\1+' $1` -o `egrep  '([A-Z])\1+' $1` ]]
+if egrep  '([0-9])\1+' $1 
 then
+	let flag=1
 	let strength=strength-10
 	echo "| -10 points for recurring consecutive digit              |"
+
+fi
+
+if [ `egrep  '([0-9])\1+' $1` -a `$flag -eq 0` ]
+then
+	let flag=1
+	let strength=strength-10
+	echo "| -10 points for recurring consecutive lc alp             |"
+
+fi
+
+if [ `egrep  '([0-9])\1+' $1` -a `$flag -eq 0` ]
+then
+	let flag=1
+	let strength=strength-10
+	echo "| -10 points for recurring consecutive uc alp             |"
 
 fi
 
