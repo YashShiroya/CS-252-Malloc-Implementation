20749dbbc4b2512af5671caa22ac3aca9ec5369d
yshiroya <yshiroya@purdue.edu>
2016-02-15 21:43:51 -0500

    Lab 2 commit

--- monitor.sh
+++ monitor.sh
@@ -10,6 +10,7 @@ git commit -a -m "Lab 2 commit" >> .local.git.out
 # cycles per second
 hertz=$(getconf CLK_TCK)
 check=0
+max_reports_counter=0
 function check_arguments {
 
 
@@ -74,7 +75,15 @@ function generate_report {
 
 	
 	#if ./reports_dir has more than $MAXIMUM_REPORTS reports, then, delete the oldest report to have room for the current one
-
+	let max_reports_counter=max_reports_counter+1
+	
+	if [ $max_reports_counter -gt $MAXIMUM_REPORTS ]
+	then
+		 remove_file=$(ls -t reports_dir | tail -1)
+		 rm $remove_file
+		 
+	fi
+	
 	#Name of the report file
 	file_name="$(date +'%d.%m.%Y.%H.%M.%S')"
 
@@ -101,7 +110,7 @@ function calculate_cpu_usage {
 	#CPU usage is measured over a periode of time. We will use the user-provided interval_time value to calculate 
 	#the CPU usage for the last interval_time seconds. For example, if interval_time is 5 seconds, then, CPU usage
 	#is measured over the last 5 seconds
-
+	
 
 	#First, get the current utime and stime (oldutime and oldstime) from /proc/{pid}/stat
 	oldutime=$(cat /proc/$PID/stat | awk '{print $14}')
@@ -139,6 +148,7 @@ function calculate_mem_usage
 function notify
 {
 	check_if=0
+	
 	#We convert the float representating the CPU usage to an integer for convenience. We will compare $usage_int to $CPU_THRESHOLD
 	cpu_usage_int=$(printf "%.f" $1)
 	mem_usage_int=$2
@@ -155,7 +165,7 @@ function notify
 	then
 		echo "Threshold reached, Sending Email to $USER"
 		latest_file=$(ls -t reports_dir | head -1)
-		/usr/bin/mailx -s "CPU Threshold reached" yadav11 < ./reports_dir/$latest_file
+		/usr/bin/mailx -s "CPU Threshold reached" $USER < ./reports_dir/$latest_file
 	fi
 
 	#Check if process exceeded its CPU or MEM thresholds. If that is the case, send an email to $USER containing the last report
